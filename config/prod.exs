848e87538cf59709ecd3eb2a45114d8dd33529cf
Serge Karpiesz <skarpiesz@gmail.com>
2017-05-22 01:23:01 +0300

    Configure Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :qa_page, QaPage.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "qa-page.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,6 +57,9 @@ config :logger, level: :info
 #     config :qa_page, QaPage.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+config :qa_page, QaPage.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
